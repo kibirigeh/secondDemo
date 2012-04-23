93acc23f382d9548c93e24e01a80fa2c44d13f1a
kibirige haruna <kibirige.haruna@gmail.com>
2012-04-22 22:45:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondDemo::Application.config.secret_token = '50cde762b106f9526a85762aee4ddbd92a18e307a1e4df5fdc609f615fe7519d8f7a065e8cb8d864686f002d63b762385cc4318522d0f5094cd233d0f941cce8'
